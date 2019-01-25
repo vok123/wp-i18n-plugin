f2dd2b6b65955844b04e46ac32c634791bac7793
vok123 <512855971@qq.com>
2019-01-25 17:24:46 +0800

    新增example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,27 +1,4 @@
-# 用于webpack 4进行多编译进行多语言打包处理. 包含编译多语言处理及异步多语言拆分输出
-  - 项目打包后根据配置的多语言数量生成多分具有单一语言的文件, 例如使用cn,en则最后输出将会有 /dist/cn , /dist/en
-
-
-### 安装
-``` bash
-npm install webpack webpack-cli wp-i18n-loader --save-dev
-```
-### example.js
-``` js
-console.log('__("你好")');
-```
-### src/lang/lang.json
-``` json
-{
-  "你好": {
-    "cn": "你好",
-    "en": "Hello"
-  }
-}
-```
-### webpack.config.js
-``` js
-const WebpackI18n = require('wp-i18n-loader'),
+const WebpackI18n = require('../index.js'),
   path = require('path'),
   _ = require('lodash');
 
@@ -46,7 +23,7 @@ let webpackConfig = {
       // 格式化对象空格数
       formatSpace: 2,
       // 是否展示未翻译的项
-      showDetail: true,
+      showDetail: false,
       // 自动移除无用的key
       removeUnUseKeys: true
     }),
@@ -58,10 +35,7 @@ let webpackConfig = {
     })
   ]
 };
-```
 
-### webpack多编译设置(使用node api 或者 webpack config中的一种)
-``` js
 // node api
 webpackConfigArr = ['cn', 'en'].map(language => {
   webpackConfig = _.cloneDeep(webpackConfig);
@@ -76,29 +50,15 @@ webpack(webpackConfigArr, (err, stats) => {
   err && console.log(err);
 });
 
-// webpack config
-module.exports = ['cn', 'en'].map((language) => {
-  webpackConfig = _.cloneDeep(webpackConfig);
-	// 配置当前编译参数
-  webpackConfig.name = language;
-  // 配置输出路径
-  webpackConfig.output.path = path.join(__dirname, 'dist') + '/' + language;
-  return webpackConfig;
-});
+// npm run test 
 
-```
+// module.exports = ['cn', 'en'].map(language => {
+//   webpackConfig = require('lodash').cloneDeep(webpackConfig);
+//   // 配置当前编译参数
+//   webpackConfig.name = language;
+//   // 配置输出路径
+//   webpackConfig.output.path = require('path').join(__dirname, 'dist') + '/' + language;
+//   return webpackConfig;
+// });
 
-### dist/en/output.js
-``` js
-console.log('Hello');
-```
-### dist/cn/output.js
-``` js
-console.log('你好');
-```
-
-### 关于多编译 webpack-dev-server 配置
-  - 建议只对单一的入口进行配置
-### 关于编译性能提升
-  - 建议开发时只启动单个语言进行开发, 以降低编译次数以达到快速编译的效果
-
+// npx webpack --config ./example/webpack.config.js
